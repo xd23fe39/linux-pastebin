0ad9ceb3d9e4d3f7d768b49f7a6160a39753a451
xd23fe39 <xd23fe39@yahoo.de>
2018-05-05 12:15:20 +0200

    Kleinere Überarbeitung

--- networking/ipaddress.sh
+++ networking/ipaddress.sh
@@ -4,3 +4,12 @@ ifconfig -a | grep 'inet addr' | cut -d: -f 2 | awk '{print $1}'
 
 # IP-Adresse von eth0 nach $ETH0
 ETH0=$(ifconfig eth0 | grep 'inet addr' | cut -d: -f 2 | awk '{print $1}')
+
+# IP4 Adressen anlisten
+ip a | grep 'inet '
+
+# IP6 Adressen anlisten
+ip a | grep 'inet6'
+
+# Offene Ports (TCP)
+netstat -tlna
