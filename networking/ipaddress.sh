b9d27861012d0ef6b0b8ff090e1a0fdd16b0e5ee
xd23fe39 <xd23fe39@yahoo.de>
2018-05-12 09:46:04 +0200

    Fix: ipadress.sh. Using ip a.

--- networking/ipaddress.sh
+++ networking/ipaddress.sh
@@ -2,6 +2,12 @@
 # Ausgabe der IP-Adressen 
 ifconfig -a | grep 'inet addr' | cut -d: -f 2 | awk '{print $1}'
 
+# Ausgabe der IPv4-Adressen
+ip a | grep 'inet ' | awk '{print $2}'
+
+# Ausgabe der IPv4 und IPv6-Adressen
+ip a | grep 'inet' | awk '{print $2}'
+
 # IP-Adresse von eth0 nach $ETH0
 ETH0=$(ifconfig eth0 | grep 'inet addr' | cut -d: -f 2 | awk '{print $1}')
 
