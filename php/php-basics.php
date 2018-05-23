9320722c75f762711b2082c0bee31a83fa1f5f64
Frank <xd23fe39@yahoo.de>
2018-05-23 12:23:49 +0200

    Create php-basics.php

---
+++ php/php-basics.php
@@ -0,0 +1,24 @@
+<?php
+
+// Simple
+$num = 5
+$float = 3.1415
+$str = "This is $name\n";
+$lit = 'This is text';
+
+// Conditions
+if ($a == $b) echo "equal"; else echo "not-equal";
+
+// Arrays
+$arr = array(1, 2, 3, 4);
+$list = array('name' => "Joe", 'mail' => "joe@mail.com");
+print_r($arr);
+$c = count($arr);
+
+// for and foreach
+for ($i = 0; $i < $cnt; $i++) echo "$i\n";
+foreach ($arr as $num) { echo "$num\n"; }
+foreach ($argv as $opt) { echo "$opt\n"; }
+
+
+?>
