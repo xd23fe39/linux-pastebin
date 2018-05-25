e57ef09a45171abb047cc7017759e35a08b66567
Frank <xd23fe39@yahoo.de>
2018-05-25 07:30:22 +0200

    Update php-basics.php

--- php/php-basics.php
+++ php/php-basics.php
@@ -1,20 +1,22 @@
 <?php
-exit(0);
+exit(0);   // Beende mit Fehlercode 0
 
-// Simple
-$num = 5
-$float = 3.1415
-$str = "This is $name\n";
-$lit = 'This is text';
+// Einfache Datentypen
+$num = 5                       // Ganzzahl int
+$float = 3.1415                // Fließkommazahl (float, double)
+$str = "This is $name\n";      // Zeichenkette
+$lit = 'This is text';         // Zeichenliteral 
 
-// Conditions
+// IF-Bedingungen ==, !=, >, <, <=, >=, &&, ||
 if ($a == $b) echo "equal"; else echo "not-equal";
 
 // Arrays
-$arr = array(1, 2, 3, 4);
-$list = array('name' => "Joe", 'mail' => "joe@mail.com");
-print_r($arr);
-$c = count($arr);
+$arr = array(1, 2, 3, 4);       // einfaches Datenfeld (Liste) von Zahlenwerten
+$list = array(                  // Hash-Array mit Name => Werte - Paaren
+    'name' => "Joe", 
+    'mail' => "joe@mail.com");
+print_r($arr);                  // Ausgabe des Array-Inhalts
+$c = count($arr);               // Anzahl Elemente des Arrays
 
 // for and foreach
 for ($i = 0; $i < $cnt; $i++) echo "$i\n";
@@ -32,5 +34,9 @@ switch ($in) {
   default:
         break;
 }
+
+// Funktionen
+
+function get_item($name = "name") { return $list[$name] };
 
 ?>
