4659906788a09337a2eaa7847944460ed7e6dbac
Frank <xd23fe39@yahoo.de>
2018-08-16 12:00:17 +0200

    Create mysql-skip-replication-error.sh

---
+++ mysql/mysql-skip-replication-error.sh
@@ -0,0 +1,27 @@
+CHECKHOST='MYSQL-SLAVE'
+
+USER=$1
+PASS=$2
+SKIP=$3
+
+HOST=$(hostname -s)
+IP=$(hostname -i)
+CMD="mysql -u root -p $PASS -e"
+
+if [ "$CHECKHOST" != "$HOST" ]; then
+    echo; echo "Kein MySQL SLAVE!"; echo
+    exit 1
+fi
+
+echo; echo "MySQL: Skip Replication Error on $HOST ($IP)"; echo
+
+if [ "$SKIP" != "yes" ]; then
+    $CMD 'SHOW STATUS\G'
+    echo; echo "  Usage: CMD user pass yes to skip error!"; echo
+    exit 2
+fi
+
+$CMD 'STOP SLAVE'
+$CMD 'SET GLOBAL SQL_SLAVE_SKIP_COUNTER = 1'
+$CMD 'STOP SLAVE'
+$CMD 'SHOW STATUS\G'
