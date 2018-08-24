7e2877e91460e80c11cc7dc0a7ebd71248a39fa3
Frank <xd23fe39@yahoo.de>
2018-08-24 12:49:23 +0200

    Create bash-arrays.sh

---
+++ lang-bash/bash-arrays.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# array=("${(@)array:#<element to remove>}")
+
+CLP=($@)               # Verwandle in Array
+CNT=${#CLP[@]}         # Anzahl Elemente von Array merken
+CLA=${CLP[@]:1:3}      # Elemente 2 bis 3 in neues Array schreiben
+CLR=${CLP[@]:2:3}      # Elemente 2 bis 3 in neues Array schreiben
+CLE=${CLP[@]:2:$CNT}   # Elemente 2 bis Ende i neues Array schreiben
+
+echo "Counter: $CNT"
+echo
+
+echo
+echo "Kommandozeilenparameter:"
+echo "---"
+echo $@
+echo "---"
+
+echo
+echo "Ergebnisausgabe"
+echo "---"
+echo $CLA
+echo $CLR
+echo $CLE
+echo "---"
+echo 
+
+echo "Foar-Each-Loop:"
+echo "---"
+for ITEM in $@; do
+    if [[ "$ITEM" == *--* ]]; then
+        continue
+    fi
+    echo -n "$ITEM "
+done
+echo
+echo "---"
+echo
