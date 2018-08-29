2797eef9cc21cee5ef7eea8df809a303ac80ee41
Frank <xd23fe39@yahoo.de>
2018-08-29 11:41:42 +0200

    Optimierung

--- lang-bash/bash-arrays.sh
+++ lang-bash/bash-arrays.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
-#
-# cmd a b c d e f g --p1 --p2
 
 # array=("${(@)array:#<element to remove>}")
 
-CLP=($@)               # Verwandle in Array
-CNT=${#CLP[@]}         # Anzahl Elemente von Array merken
-CLA=${CLP[@]:1:3}      # Elemente 2 bis 3 in neues Array schreiben
-CLR=${CLP[@]:2:3}      # Elemente 2 bis 3 in neues Array schreiben
-CLE=${CLP[@]:2:$CNT}   # Elemente 2 bis Ende i neues Array schreiben
+CNT=${#@}         # Anzahl Elemente von Array merken
+CLA=${@:1:3}      # Elemente 2 bis 3 in neues Array schreiben
+CLR=${@:2:3}      # Elemente 2 bis 3 in neues Array schreiben
+CLE=${@:2:$CNT}   # Elemente 2 bis Ende i neues Array schreiben
 
 echo "Counter: $CNT"
 echo
