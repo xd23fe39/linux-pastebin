19c7d2f4dff9bb5f1c1584ef45536b3b5b48a457
Frank <xd23fe39@yahoo.de>
2018-08-29 11:42:55 +0200

    Fehler in Kommentar

--- lang-bash/bash-arrays.sh
+++ lang-bash/bash-arrays.sh
@@ -3,7 +3,7 @@
 # array=("${(@)array:#<element to remove>}")
 
 CNT=${#@}         # Anzahl Elemente von Array merken
-CLA=${@:1:3}      # Elemente 2 bis 3 in neues Array schreiben
+CLA=${@:1:3}      # Elemente 1 bis 3 in neues Array schreiben
 CLR=${@:2:3}      # Elemente 2 bis 3 in neues Array schreiben
 CLE=${@:2:$CNT}   # Elemente 2 bis Ende i neues Array schreiben
 
