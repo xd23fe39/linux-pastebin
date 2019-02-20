38e196d4e6847143b63605457ea611c49bbcc172
xd23fe39 <xd23fe39@yahoo.de>
2019-02-20 19:22:53 +0100

    curl-Tag hinzugefügt

--- curl/curl-pastebin.sh
+++ curl/curl-pastebin.sh
@@ -5,3 +5,6 @@ curl -v --use-ascii --crlf -T /home/myuser/DATA/mydata.dat --ftp-ssl  -1 --cacer
 # cURL und HTTPS
 curl -v --cacert /var/certs/cacert.crt  "https://mysslhost.com"
  
+# Herunterladen ausführen eines Scriptes. Das Skript wird per https:// bereitgestellt,
+# was eine gewisse vertrauenswürdige Umgebung erfordert.
+curl -sSL https://localhost/install/script.txt | bash
