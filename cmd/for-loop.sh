73bfa7358b25594e9882f5f36f6937d5cf5e22f6
xd23fe39 <xd23fe39@yahoo.de>
2019-03-10 10:02:07 +0100

    Beispiele hinzugefügt

--- cmd/for-loop.sh
+++ cmd/for-loop.sh
@@ -7,5 +7,13 @@ do
    echo $datei
 done
 
+# Zählschleife im C-Stil
+for ((i=0; i < 10; i++)); do
+  echo $i
+done
+
+# Loop über alle Elemente der Liste
+for i in ${LISTE[*]}; do echo $i; done
+
 # Quelle:
 # http://www.tldp.org/LDP/abs/html/arrays.html
