eb1791f0a0a74d9b4f0e3169add1faebb169a57e
xd23fe39 <xd23fe39@yahoo.de>
2019-04-05 20:28:37 +0200

    Erweitert

--- cmd/find.sh
+++ cmd/find.sh
@@ -2,8 +2,14 @@
 man find
 exit 0
 
-# Lösche alle Dateien, die älter als 90 Tage sind
+# Lösche alle Dateien, die älter als 90 Tage sind (Action: -delete -exec command -ls -ok command )
 find /var/log/mysql -mtime +90 -type f -delete
 
-# Suche nach Datei
+# Anlisten aller Dateien gem. FIND-Suchfilter 
+find /tmp -name '*cpu*' -ls
+
+# Zeigt eine Liste mit Dateien und Verzeichnissen die xxx im Namen enthalten
 find /tmp -name "*xxx*"
+
+# Zeigt eine Liste von Dateien mit xxx im Dateinamen
+find /tmp -type f -name "*xxx*"
