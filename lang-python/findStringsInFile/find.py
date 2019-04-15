79e26d5fdda7cfb3aa366ed55ce02962bf5a31da
Frank <xd23fe39@yahoo.de>
2019-04-15 11:28:11 +0200

    Create find.py

---
+++ lang-python/findStringsInFile/find.py
@@ -0,0 +1,32 @@
+# Python Hilfsscript für die Zeichenkettenzerlegung
+#
+# C:\Users\xcb3504\AppData\Local\Programs\Python\Python36-32\python.exe
+
+# Teilzeichenkette für Suche (Wortanfang)
+SEARCH_PATTERN = "XXXX"
+
+print("\nSearch Pattern: %s\n" % SEARCH_PATTERN)
+
+##
+# Parsen einer Zeile / Zeichenkette
+#
+def parser(line):
+    lsize = len(line)
+    print("Parser Input: %d chars\n" % (lsize))
+    for i in range(lsize):
+        smatch = line[i:i+3]                  # hole die nächsten 3 Zeichen aus line
+        if smatch == SEARCH_PATTERN:          # suche nach Pattern. Falls gefunden, dann
+            pstr = line[i:i+55]               # hole 55 Zeichen aus line nach pstr
+            pos = pstr.index("<")             # suche nach Wort-Ende '<' (Position)
+            tokken = pstr[0:pos]              # extrahiere das Tokken
+            print("%s" % (tokken))        # und gebe es aus 
+
+##
+# Hauptprogramm
+#
+fobj = open("data.txt", "r")
+for line in fobj:
+    # print(line.rstrip())                    # Ausgabe einer Zeile aus der Datei fobj
+    parser(line)                              # Line-Parser aufrufen
+fobj.close()
+
