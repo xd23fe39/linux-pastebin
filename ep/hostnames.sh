73b84fd7e9b1118ae8df6a8afe17b658b8093a57
Frank <xd23fe39@yahoo.de>
2019-09-24 13:12:45 +0200

    Update hostnames.sh

--- ep/hostnames.sh
+++ ep/hostnames.sh
@@ -7,3 +7,5 @@ grep $PRE $INFILE | cut -d '@' -f 2 | cut -d "'" -f 1 | cut -d ' ' -f 1 >> $INFI
 # Ersetze alle Zeilen mit 'alias' durch '---'
 sed 's/alias/---/' $INFILE.out >> $INFILE.out1 
 
+# Ausgabe der Art HOSTNAME IPADDRESS 
+for i in $(cat $INFILE.out1); do if [ $i != '---' ]; then echo -n $i " "; nslookup $i | grep Address | tail -1 | cut -d ' ' -f 2; fi; done;
