bae0c3189043712d9220bb48147ffb1b3ea02976
xd23fe39 <xd23fe39@yahoo.de>
2019-12-24 01:28:45 +0100

    Kleine Verbesserungen

--- lang-python/sandbox/build.py
+++ lang-python/sandbox/build.py
@@ -10,6 +10,10 @@ I_WORKING_DIR = os.getcwd()
 I_GETENV_USER = os.getenv("USER")
 I_SYS_SCRIPTNAME = sys.argv[0]
 
+G_APPLICATION_NAME = "Generic"
+G_APPLICATION_VERSION = "1.0.1"
+G_APPLICATION_RELEASE = "19.12.130"
+
 ##########################################################################
 # Basic Operations
 
@@ -28,6 +32,7 @@ def deploy():
 if __name__ == "__main__":
     pass
 
+print("{:s}, {:s} ({:s})\n----".format(G_APPLICATION_NAME, G_APPLICATION_VERSION, G_APPLICATION_RELEASE))
 print("Current working directory: {:s}".format(I_WORKING_DIR))
 print("Current user name: {:s}".format(I_GETENV_USER))
 
@@ -36,6 +41,8 @@ try:
     print("Operation: {}".format(oper))
     eval(oper)
     exit(0)
+except IndexError:
+    pass
 except NameError:
     print("Operation nicht gefunden.".format(sys.argv[1]))
 finally:
